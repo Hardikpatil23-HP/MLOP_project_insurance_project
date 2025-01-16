802c0b93fd024bb87e9428dd079be9b93faf003f
Hardik Patil <hardikmpatil23@gmail.com>
2025-01-16 15:28:06 +0530

    second commit

--- src/entity/s3_estimator.py
+++ src/entity/s3_estimator.py
@@ -1,72 +1,62 @@
-from azure_storage import AzureBlobStorageService
+from src.cloud_storage.aws_storage import SimpleStorageService
 from src.exception import MyException
 from src.entity.estimator import MyModel
 import sys
 from pandas import DataFrame
-import os
 
 
-class Proj1EstimatorAzure:
+class Proj1Estimator:
     """
-    This class is used to save and retrieve our model from Azure Blob Storage and to do predictions.
+    This class is used to save and retrieve our model from s3 bucket and to do prediction
     """
 
-    def __init__(self, connection_string: str, container_name: str, model_path: str):
+    def __init__(self,bucket_name,model_path,):
         """
-        :param connection_string: Azure Blob Storage connection string.
-        :param container_name: Name of your container in Azure Blob Storage.
-        :param model_path: Location of your model blob in the container.
+        :param bucket_name: Name of your model bucket
+        :param model_path: Location of your model in bucket
         """
-        self.connection_string = connection_string
-        self.container_name = container_name
+        self.bucket_name = bucket_name
+        self.s3 = SimpleStorageService()
         self.model_path = model_path
-        self.azure_storage = AzureBlobStorageService(connection_string)
-        self.loaded_model: MyModel = None
+        self.loaded_model:MyModel=None
 
-    def is_model_present(self) -> bool:
-        """
-        Check if the model blob exists in the Azure container.
-        :return: True if the blob exists, False otherwise.
-        """
+
+    def is_model_present(self,model_path):
         try:
-            return self.azure_storage.blob_exists(self.container_name, self.model_path)
+            return self.s3.s3_key_path_available(bucket_name=self.bucket_name, s3_key=model_path)
         except MyException as e:
             print(e)
             return False
 
-    def load_model(self) -> MyModel:
+    def load_model(self,)->MyModel:
         """
-        Load the model from the Azure Blob Storage.
-        :return: The loaded MyModel object.
+        Load the model from the model_path
+        :return:
+        """
+
+        return self.s3.load_model(self.model_path,bucket_name=self.bucket_name)
+
+    def save_model(self,from_file,remove:bool=False)->None:
+        """
+        Save the model to the model_path
+        :param from_file: Your local system model path
+        :param remove: By default it is false that mean you will have your model locally available in your system folder
+        :return:
         """
         try:
-            temp_file_path = "temp_model.pkl"
-            self.azure_storage.download_blob(self.container_name, self.model_path, temp_file_path)
-            with open(temp_file_path, "rb") as model_file:
-                model = pickle.load(model_file)
-            os.remove(temp_file_path)
-            return model
+            self.s3.upload_file(from_file,
+                                to_filename=self.model_path,
+                                bucket_name=self.bucket_name,
+                                remove=remove
+                                )
         except Exception as e:
             raise MyException(e, sys)
 
-    def save_model(self, from_file: str, remove: bool = False) -> None:
-        """
-        Save the model to the Azure Blob Storage.
-        :param from_file: Path to the local model file.
-        :param remove: If True, remove the local file after upload.
-        """
-        try:
-            self.azure_storage.upload_file(from_file, self.container_name, self.model_path)
-            if remove:
-                os.remove(from_file)
-        except Exception as e:
-            raise MyException(e, sys)
 
-    def predict(self, dataframe: DataFrame):
+    def predict(self,dataframe:DataFrame):
         """
-        Make predictions using the loaded model.
-        :param dataframe: Input DataFrame for predictions.
-        :return: Predictions from the model.
+        :param dataframe:
+        :return:
         """
         try:
             if self.loaded_model is None:
